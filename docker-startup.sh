cd119983322fe6cc010971ea742928e5eac70b20
why <529570509@qq.com>
2017-06-02 11:59:48 +0800

    debug

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sudo docker run -it -d -v /home/suntopo/Recog/:/Recog -p 20001:20001 --name pycv0.3 4217bf3ea7d4 bash -c "cd /Recog/recog/ && python MainApplication.py"
-
-ssh -t suntopo@120.77.80.118 bash -c "cd Recog/recog/ && git pull && sudo docker restart pycv0.3"
+ssh -t suntopo@120.77.80.118 "bash -c 'cd Recog/recog/ && git pull && sudo docker restart pycv0.3'"
