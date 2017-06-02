a6e4d1fd168cb3d152a8d002d95f281672961d49
why <529570509@qq.com>
2017-06-02 11:34:52 +0800

    startup sh

---
+++ docker-startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# sudo docker run -it -d -v /home/suntopo/Recog/:/Recog -p 20001:20001 --name pycv0.3 4217bf3ea7d4 bash -c "cd /Recog/recog/ && python MainApplication.py"
+
+ssh -t suntopo@120.77.80.118 bash -c "cd Recog/recog/ && git pull && sudo docker restart pycv0.3"
